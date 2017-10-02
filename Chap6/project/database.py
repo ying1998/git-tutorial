aafeab7fd05bd12ccac83964872237bbbdb0eaaa
ying1998 <yingtuo1998@qq.com>
2017-10-02 08:41:09 +0800

    insert data

--- Chap6/project/database.py
+++ Chap6/project/database.py
@@ -1,10 +1,22 @@
 import sqlite3 as lite
 import sys
 
-con = lite.connect('weather.db')
+def createdata():
+    con = lite.connect('weather.db')
 
-with con:
-    cur = con.cursor()
+    with con:
+        cur = con.cursor()
 
-    cur.execute('DROP table if exists Weathers')
-    cur.execute('CREATE table weather(day text,location text,weather text,temper  text)')
+        cur.execute('DROP table if exists SeachWeathers')
+        cur.execute('CREATE table SeachWeathers(day text,location text,weather text,temper  text)')
+
+
+def insertdata(day,location,weather,temper):
+    con = lite.connect('weather.db')
+    with con:
+        cur = con.cursor()
+        cur.execute('INSERT into SeachWeathers values(?,?,?,?)',(day, location,weather,temper))
+
+if __name__ == '__main__':
+    createdata()
+    insertdata('2017-10-2','beijing','cloud','30')
